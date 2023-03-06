0ceeadef107ba80dfae209ae8fb445ddc581a999
JusticeBrains <justiceduodu14@gmail.com>
2023-03-06 17:25:09 +0000

    confiure swagger

--- config/settings.py
+++ config/settings.py
@@ -160,3 +160,7 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS':['django_filters.rest_framework.DjangoFilterBackend'],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination","PAGE_SIZE": 50,
 }
+
+SWAGGER_SETTINGS = {
+   'DEFAULT_INFO': 'config.urls.api_info',
+}
