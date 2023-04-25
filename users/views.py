3fe544382781ca014dbbe8b3d530c01796f1c5dc
JusticeBrains <justiceduodu14@gmail.com>
2023-04-25 15:30:30 +0000

    refactored

--- users/views.py
+++ users/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from django.contrib.auth import get_user_model
 from rest_framework import viewsets
 
-from .serializers import UserSerializer
+from .serializers import CustomUserSerializer
 
 User = get_user_model()
 
 class UserViewset(viewsets.ModelViewSet):
     queryset = User.objects.all()
-    serializer_class = UserSerializer
+    serializer_class = CustomUserSerializer
