5bb9795a19025a1fd6db8662c7ecb8b27d6d9ff1
JusticeBrains <justiceduodu14@gmail.com>
2023-04-25 15:28:55 +0000

    refactored updated

--- users/models.py
+++ users/models.py
@@ -1,18 +1,31 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, PermissionsMixin, AbstractBaseUser
 from django.utils.translation import gettext_lazy as _
-
 from options.text_options import ASSIGNEDAREA
+from django.utils import timezone
+import uuid
+
+from .managers import CustomUserManager
 
 
-class CustomUser(AbstractUser):
-    fullname = models.CharField(_("Fullname"), max_length=50, blank=True, null=True)
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    id = models.UUIDField(_("Id"), primary_key=True, default=uuid.uuid4, editable=False)
+    username = models.CharField(_("Fullname"), max_length=50, blank=True, null=True)
+    email = models.EmailField(_("Email"), max_length=254, unique=True)
     profile_pic = models.ImageField(_("Profile Pic"),
                                     upload_to='users/profile_pic',
                                     height_field=None, width_field=None,
                                     max_length=None, null=True, blank=True)
     assigned_Area = models.CharField(_("Assigned Area"), max_length=50, choices=ASSIGNEDAREA.choices)
     received_notifications = models.BooleanField(_("Received Notifications"), blank=True, null=True)
+    is_active = models.BooleanField(_("Is Active"), default=True)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(_("Date Joined"), default=timezone.now)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username',]
+
+    objects = CustomUserManager()
 
     class Meta:
         verbose_name = "User"
