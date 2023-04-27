c7394e813863fc8e5cf0668df44a957cc03289ed
JusticeBrains <justiceduodu14@gmail.com>
2023-04-27 11:23:19 +0000

    refactored and replace AbstractBaseUser to AbstractUser

--- users/serializers.py
+++ users/serializers.py
@@ -7,4 +7,4 @@ User = get_user_model()
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['username', 'email','password']
+        exclude = ['user_permissions', 'groups','is_superuser']
