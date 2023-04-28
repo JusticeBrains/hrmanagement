67e044ba07e51e3cb480d01aa746d7e3895b4859
JusticeBrains <justiceduodu14@gmail.com>
2023-04-28 11:02:00 +0000

    refactored

--- users/models.py
+++ users/models.py
@@ -8,16 +8,24 @@ import uuid
 from .managers import CustomUserManager
 
 
-class CustomUser(AbstractUser):
+class CustomUser(AbstractBaseUser, PermissionsMixin):
     id = models.UUIDField(_("Id"), primary_key=True, default=uuid.uuid4, editable=False)
+    username = models.CharField(_("Username"), max_length=50, unique=True, blank=True, null=True)
     assigned_Area = models.CharField(_("Assigned Area"), max_length=50, choices=ASSIGNEDAREA.choices, null=True, blank=True)
     email = models.EmailField(_("Email"), max_length=254, unique=False)
-    received_notifications = models.BooleanField(_("Received Notifications"), blank=True, null=True)
     first_name = models.CharField(_("First Name"), max_length=50, blank=True, null=True)
     last_name = models.CharField(_("Last Name"), max_length=50, blank=True, null=True)
     is_super = models.PositiveIntegerField(_("Is Super"), default=0)
     is_hr = models.PositiveIntegerField(_("Is Hr"), default=0)
+    is_active = models.BooleanField(_("Is Active"), default=True)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(_("Date Joined"), default=timezone.now)
+    is_verified = models.CharField(_("Is Verified"), max_length=50, blank=True, null=True, default=0)
+    
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email',]
 
+    objects = CustomUserManager()
 
     class Meta:
         verbose_name = "User"
