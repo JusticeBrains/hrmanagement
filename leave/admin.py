ec9ddc45dcd8d60f33174f08ab14a1a4c7b15bf0
JusticeBrains <justiceduodu14@gmail.com>
2023-04-28 10:25:13 +0000

    updated

--- leave/admin.py
+++ leave/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from leave import models as lmodels
 
-# Register your models here.
+
+admin.site.register(lmodels.Policy)
+admin.site.register(lmodels.Assignment)
+admin.site.register(lmodels.LeaveRequest)
+admin.site.register(lmodels.LeaveTransaction)
+admin.site.register(lmodels.LeavePlan)
+admin.site.register(lmodels.LeaveType)
