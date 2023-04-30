2de33093e46a5e8145d8ae503c3866f99b1196aa
JusticeBrains <justiceduodu14@gmail.com>
2023-04-30 20:43:37 +0000

    branch, unit and department register in admin model

--- employee/admin.py
+++ employee/admin.py
@@ -4,3 +4,6 @@ from employee import models as emodels
 
 admin.site.register(emodels.Employee)
 admin.site.register(emodels.StaffCategory)
+admin.site.register(emodels.Branch)
+admin.site.register(emodels.Unit)
+admin.site.register(emodels.Department)
