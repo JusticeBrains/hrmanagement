7a7abc8a4276d076bdad628d0d3b269e643187cf
JusticeBrains <justiceduodu14@gmail.com>
2023-05-01 17:32:08 +0000

    leavetpe url added

--- leave/urls.py
+++ leave/urls.py
@@ -1,8 +1,10 @@
 from rest_framework.routers import SimpleRouter
 
-from .views import  LeaveRequestViewSet
+from .views import  LeaveRequestViewSet, LeaveTypeViewSet
 
 router = SimpleRouter()
 router.register('leaverequest', LeaveRequestViewSet, basename='leaverequest')
+router.register('leavetype', LeaveTypeViewSet, basename='leavetype')
+
 
 urlpatterns = router.urls
