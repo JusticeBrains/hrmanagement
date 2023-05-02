b0e0767b4ac582911951e43dcbf56256d7304244
JusticeBrains <justiceduodu14@gmail.com>
2023-05-02 13:22:38 +0000

    leaveplan serializer implemented

--- leave/serializers.py
+++ leave/serializers.py
@@ -6,6 +6,7 @@ from .models import (
     LeaveLimits,
     LeaveType,
     LeaveRequest,
+    LeavePlan
 )
 
 
@@ -28,6 +29,25 @@ class LeaveRequestSerializer(serializers.ModelSerializer):
             days_added += 1
         return start_date
 
+
+class LeavePlanSerializer(serializers.ModelSerializer):
+    end_date = serializers.SerializerMethodField()
+    class Meta:
+        model = LeavePlan
+        fields = "__all__"
+
+    def get_end_date(self, obj):
+        """custom method to compute duration"""
+        current_date = datetime.now().date()
+        start_date = max(obj.start_date, current_date)
+        days_added = 0
+
+        while days_added < obj.no_of_days_requested:
+            start_date += timedelta(days=1)
+            if start_date.weekday() >= 5:
+                continue
+            days_added += 1
+        return start_date
 
 
 
