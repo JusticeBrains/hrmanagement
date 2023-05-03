68d2c1f4bddb59a885727e2762829a51ec25c2ee
JusticeBrains <justiceduodu14@gmail.com>
2023-05-03 01:31:25 +0000

    jobtitles and salarygrade register

--- company/admin.py
+++ company/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Company, CompanyType, Job
+from .models import Company, CompanyType, Job, JobTitles, SalaryGrade
 
 
 @admin.register(Company)
@@ -15,3 +15,6 @@ class CompanyTypeAdmin(admin.ModelAdmin):
 @admin.register(Job)
 class JobAdmin(admin.ModelAdmin):
     list_display = ('comp_code', 'job_code','job_title',)
+
+admin.site.register(JobTitles)
+admin.site.register(SalaryGrade)
