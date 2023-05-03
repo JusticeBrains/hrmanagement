3f9914e82eae58bd91846e7a8dad5eef71dcee8d
JusticeBrains <justiceduodu14@gmail.com>
2023-05-03 01:31:48 +0000

    jobtitles and salarygrade loader implemented

--- employee/management/commands/load_data.py
+++ employee/management/commands/load_data.py
@@ -1,5 +1,6 @@
 from typing import Any, Optional
 from django.core.management.base import BaseCommand
+from company.models import SalaryGrade, JobTitles
 from employee.models import Department, Branch, Unit
 import json
 from pprint import pprint
@@ -18,6 +19,11 @@ class Command(BaseCommand):
         with open("employee/unit.json", 'r') as file:
             unit = json.load(file)
 
+        with open("employee/jobtitles.json", 'r') as file:
+            jobtitles = json.load(file)
+
+        with open("employee/salarylevel.json", 'r') as file:
+            salarylevel = json.load(file)
 
         print(f"--Loading Departments --")
         for val in dep["value"]:
@@ -56,3 +62,31 @@ class Command(BaseCommand):
                 )
             print(f"--End-- {val['Name']}")
         print(f"Done -- Unit -- {Unit.objects.all().count()}")
+
+
+        print(f"--Loading JobTitles --")
+        for val in jobtitles["value"]:
+            print(f"--Startng--{val['Code']} ")
+            if not JobTitles.objects.filter(code=val['Code']):
+                JobTitles.objects.create(
+                    code=val["Code"],
+                    # payroll_structure=val["Payroll_Structure"],
+                    # salary_grade=val["Salary_Grade"],
+                    description=val['Description']
+                )
+            print(f"--End-- {val['Description']}")
+        print(f"Done -- Unit -- {JobTitles.objects.all().count()}")
+
+
+        print(f"--Loading SalaryGrade --")
+        for val in salarylevel["value"]:
+            print(f"--Startng--{val['Code']} ")
+            if not SalaryGrade.objects.filter(code=val['Code']):
+                SalaryGrade.objects.create(
+                    code=val["Code"],
+                    payroll_structure=val["Payroll_Structure_Code"],
+                    job_tiles=val["Job_Titles"],
+                    transport_rate=val['Transport_Rate']
+                )
+            print(f"--End-- {val['Code']}")
+        print(f"Done -- Unit -- {SalaryGrade.objects.all().count()}")
