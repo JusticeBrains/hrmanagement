3cf0aae655d0c4c6c43ca270ff36a4bdba7a694b
JusticeBrains <justiceduodu14@gmail.com>
2023-05-05 03:47:50 +0000

    start_date changed

--- leave/models.py
+++ leave/models.py
@@ -21,7 +21,7 @@ class LeaveBase(models.Model):
         null=True,
         blank=True,
     )
-    start_date = models.DateField(_("Start Date"), default=timezone.now())
+    start_date = models.DateField(_("Start Date"), default=timezone.now)
     no_of_days_requested = models.PositiveIntegerField(_("No Of Days Requested"))
     job_description = models.CharField(
         _("Job Description"), max_length=150, null=True, blank=True
