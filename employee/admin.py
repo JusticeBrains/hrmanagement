0e81a95e777423602e9dd07090f44483dfc16f5a
JusticeBrains <justiceduodu14@gmail.com>
2023-05-11 12:21:41 +0000

    refactored

--- employee/admin.py
+++ employee/admin.py
@@ -2,10 +2,17 @@ from django.contrib import admin
 
 from employee import models as emodels
 
-admin.site.register(emodels.Employee)
+@admin.register(emodels.Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    search_fields = ['code']
+
 admin.site.register(emodels.StaffCategory)
 admin.site.register(emodels.Branch)
 admin.site.register(emodels.Unit)
 admin.site.register(emodels.Department)
 admin.site.register(emodels.Notch)
 admin.site.register(emodels.PayCategoryList)
+admin.site.register(emodels.SelfAppraisalResponse)
+admin.site.register(emodels.EmployeeAppraisalDetail)
+admin.site.register(emodels.AppraisalGrading)
+admin.site.register(emodels.EmployeeAppraisal)
