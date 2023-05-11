0e3e3aa01206f4f223287affebf0f9a36f4fd13a
JusticeBrains <justiceduodu14@gmail.com>
2023-05-11 12:21:59 +0000

    appraisal models implemented

--- employee/models.py
+++ employee/models.py
@@ -1,4 +1,3 @@
-
 import uuid
 from options.text_options import (
     GENDER,
@@ -16,6 +15,8 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth import get_user_model
 import uuid
+from django.utils import timezone
+
 User = get_user_model()
 
 
@@ -169,12 +170,21 @@ class Employee(models.Model):
         null=True,
         blank=True,
     )
-    days_left = models.PositiveIntegerField(_("Days Left"), null=True, blank=True, default=0)
-    no_of_days_exhausted = models.PositiveIntegerField(_("No. Of Days Exhausted"), blank=True, null=True)
-    plan_days_left = models.PositiveIntegerField(_("Plan Days Left"), null=True, blank=True, default=0)
-    plan_no_of_days_exhausted = models.PositiveIntegerField(_("Plan No. Of Days Exhausted"), blank=True, null=True)
-    total_number_of_leave_days = models.PositiveIntegerField(_("Total Number Of Leave Days"), null=True, blank=True)
-
+    days_left = models.PositiveIntegerField(
+        _("Days Left"), null=True, blank=True, default=0
+    )
+    no_of_days_exhausted = models.PositiveIntegerField(
+        _("No. Of Days Exhausted"), blank=True, null=True
+    )
+    plan_days_left = models.PositiveIntegerField(
+        _("Plan Days Left"), null=True, blank=True, default=0
+    )
+    plan_no_of_days_exhausted = models.PositiveIntegerField(
+        _("Plan No. Of Days Exhausted"), blank=True, null=True
+    )
+    total_number_of_leave_days = models.PositiveIntegerField(
+        _("Total Number Of Leave Days"), null=True, blank=True
+    )
 
     class Meta:
         verbose_name = "Employee"
@@ -194,77 +204,106 @@ class Employee(models.Model):
         return f"{self.last_name}, {self.first_name}"
 
 
-class AppraisalAreas(models.Model):
-    description = models.CharField(_("Description"), max_length=50)
-    scored = models.BooleanField(_("Scored"))
-
-    class Meta:
-        verbose_name = "Appraisal Areas"
-        verbose_name_plural = "Appraisal Areas"
-
-
 class EmployeeAppraisal(models.Model):
-    emp_code = models.ForeignKey(
+    emp_id = models.ForeignKey(
         Employee,
-        verbose_name=_("Employee Code"),
+        verbose_name=_("Employee"),
         on_delete=models.CASCADE,
         related_name="emp_code",
     )
-    emp_name = models.CharField(verbose_name=_("Employee Name"), max_length=50)
-    paygroup_code = models.ForeignKey(
-        "paygroup.PayGroup",
-        verbose_name=_("Pay Groups"),
-        on_delete=models.CASCADE,
-        related_name="paygroup_code",
+    emp_name = models.CharField(
+        verbose_name=_("Employee Name"), max_length=150, null=True, blank=True, editable=False
     )
-    job_title_code = models.ForeignKey(
-        "company.JobTitles",
-        verbose_name=_("Job Title"),
-        on_delete=models.CASCADE,
-        blank=True,
-        null=True,
-    )
-    job_title = models.CharField(_("Job Title"), max_length=150)
-    appraisal_date = models.DateField(
-        _("Appraisal Date"), auto_now=False, auto_now_add=False, blank=True, null=True
-    )
-    appraisal_venue = models.CharField(
-        _("Appraisal Venue"), max_length=50, blank=True, null=True
-    )
+    employee_code = models.CharField(_("Employee Code"), max_length=50, blank=True, null=True)
+    job_title = models.CharField(_("Job Title"), max_length=150, null=True, blank=True, editable=False)
+    appraisal_date = models.DateField(_("Appraisal Date"), default=timezone.now)
     appraiser = models.CharField(
         verbose_name="Appraiser", max_length=200, blank=True, null=True
     )
-    transaction_date = models.DateField(
-        _("Transaction Date"), auto_now=False, auto_now_add=False, blank=True, null=True
+    department = models.CharField(
+        _("Department"), max_length=150, null=True, blank=True, editable=False
     )
-    user_id = models.CharField(_("User ID"), max_length=50)
-    posted = models.BooleanField(_("Posted"), blank=True, null=True)
+    grade = models.CharField(_("Grade"), max_length=150, null=True, blank=True)
+    performance_score = models.DecimalField(
+        _("Performance Score"), max_digits=3, decimal_places=2, null=True, blank=True
+    )
+    percentage_score = models.DecimalField(
+        _("Percentage Score"), max_digits=3, decimal_places=2, null=True, blank=True
+    )
+    period = models.CharField(_("Period"), max_length=50, blank=True, null=True)
+    user_id = models.CharField(_("User ID"), max_length=50, blank=True, null=True)
 
     class Meta:
         verbose_name = "Employee Appraisal"
         verbose_name_plural = "Employee Appraisals"
 
+    def __str__(self) -> str:
+        return f"{self.emp_name} {self.employee_code} - {self.grade}"
+    
 
-class EmployeeAppraisalResponse(models.Model):
-    appraisal_no = models.CharField(_("Appraisal No."), max_length=50)
-    emp_code = models.CharField(verbose_name=_("Employee"), max_length=50)
+    def clean(self):
+        if self.emp_code:
+            self.emp_name = self.emp_id.fullname
+            self.job_title = self.emp_id.job_titles
+            self.department = self.emp_id.second_category_level
+            self.employee_code = self.emp_id.code
+
+
+class AppraisalGrading(models.Model):
+    mininum_score = models.PositiveIntegerField(
+        _("Minimum Score"), blank=True, null=True
+    )
+    maximum_score = models.PositiveIntegerField(
+        _("Maximum Score"), blank=True, null=True
+    )
+    grade = models.CharField(_("Grade"), max_length=50, null=True, blank=True)
+    recommendation = models.CharField(
+        _("Recommendation"), max_length=150, blank=True, null=True
+    )
+
+    class Meta:
+        verbose_name = "Appraisal Grading"
+        verbose_name_plural = "Appraisal Gradings"
+
+    def __str__(self) -> str:
+        return f"{self.grade} - {self.recommendation}"
+
+
+class EmployeeAppraisalDetail(models.Model):
+    emp_code = models.CharField(
+        _("Employee Code"), max_length=150, null=True, blank=True
+    )
+    period = models.CharField(_("Period"), max_length=150, null=True, blank=True)
+    kpi_appraisal_area = models.CharField(
+        _("KPI/Appraisal Areas"), max_length=250, blank=True, null=True
+    )
+    score = models.DecimalField(_("Score"), max_digits=3, decimal_places=2)
+    narration = models.CharField(_("Narration"), max_length=250)
+
+    class Meta:
+        verbose_name = "Employee Appraisal Detail"
+        verbose_name_plural = "Employee Appraisal Details"
+
+    def __str__(self):
+        return f"{self.emp_code} - {self.period} - {self.score}"
+
+
+class SelfAppraisalResponse(models.Model):
+    emp_details = models.ForeignKey(
+        "employee.EmployeeAppraisalDetail", verbose_name=_(""), on_delete=models.CASCADE
+    )
     appraisal_description = models.TextField(_("Appraisal Description"))
-    line_no = models.PositiveIntegerField(_("Line No"))
     question = models.CharField(_("Question"), max_length=250)
     score = models.PositiveIntegerField(_("Score"))
-    grading = models.CharField(
-        _("Grading"), choices=APPRAISALGRADES.choices, max_length=50
-    )
     answer = models.CharField(_("Answer"), max_length=250)
     comment = models.CharField(_("Comment"), max_length=250)
-    scored = models.BooleanField(_("Scored"))
 
     class Meta:
         verbose_name = "Employee Appraisal Response"
         verbose_name_plural = "Employee Appraisal Responses"
 
     def __str__(self) -> str:
-        return f"{self.appraisal_code}, {self.emp_code}"
+        return f"{self.emp_details}, {self.emp_code}"
 
 
 class EmployeePromotion(models.Model):
@@ -428,7 +467,6 @@ class EmployeeDisciplinaryActions(models.Model):
         return f"{self.emp_name}, {self.disciplinary_code}, {self.recommended_action}"
 
 
-
 class EmployeePayReview(models.Model):
     no = models.CharField(_("Code"), max_length=50)
     review_type = models.CharField(
@@ -472,7 +510,6 @@ class EmployeePayReview(models.Model):
         return self.new_base_pay
 
 
-
 class Base(models.Model):
     # id = models.UUIDField(_("ID"), primary_key=True, editable=False, default=uuid.uuid4)
     code = models.CharField(_("Code"), max_length=50, unique=True, primary_key=True)
@@ -492,19 +529,23 @@ class StaffCategory(Base):
     def __str__(self):
         return f"{self.code} - {self.name} - {self.max_number_of_days}"
 
+
 # Set the data type of the primary key to VARCHAR in PostgreSQL
-if connection.vendor == 'postgresql':
+if connection.vendor == "postgresql":
     with connection.cursor() as cursor:
-        cursor.execute("""
+        cursor.execute(
+            """
             SELECT EXISTS(
                 SELECT * FROM information_schema.columns
                 WHERE table_name = 'employee_staffcategory' AND column_name = 'code'
             )
-        """)
+        """
+        )
         column_exists = cursor.fetchone()[0]
         if column_exists:
-            cursor.execute("ALTER TABLE employee_staffcategory ALTER COLUMN code TYPE VARCHAR(50)")
-
+            cursor.execute(
+                "ALTER TABLE employee_staffcategory ALTER COLUMN code TYPE VARCHAR(50)"
+            )
 
 
 class Department(Base):
@@ -518,19 +559,23 @@ class Department(Base):
 
     def __str__(self):
         return f"{self.code} - {self.first_category_code}"
-    
 
-if connection.vendor == 'postgresql':
+
+if connection.vendor == "postgresql":
     with connection.cursor() as cursor:
-        cursor.execute("""
+        cursor.execute(
+            """
             SELECT EXISTS(
                 SELECT * FROM information_schema.columns
                 WHERE table_name = 'employee_department' AND column_name = 'code'
             )
-        """)
+        """
+        )
         column_exists = cursor.fetchone()[0]
         if column_exists:
-            cursor.execute("ALTER TABLE employee_department ALTER COLUMN code TYPE VARCHAR(50)")
+            cursor.execute(
+                "ALTER TABLE employee_department ALTER COLUMN code TYPE VARCHAR(50)"
+            )
 
 
 class Unit(Base):
@@ -546,18 +591,21 @@ class Unit(Base):
         return f"{self.code} - {self.second_category_code}"
 
 
-if connection.vendor == 'postgresql':
+if connection.vendor == "postgresql":
     with connection.cursor() as cursor:
-        cursor.execute("""
+        cursor.execute(
+            """
             SELECT EXISTS(
                 SELECT * FROM information_schema.columns
                 WHERE table_name = 'employee_unit' AND column_name = 'code'
             )
-        """)
+        """
+        )
         column_exists = cursor.fetchone()[0]
         if column_exists:
-            cursor.execute("ALTER TABLE employee_unit ALTER COLUMN code TYPE VARCHAR(50)")
-
+            cursor.execute(
+                "ALTER TABLE employee_unit ALTER COLUMN code TYPE VARCHAR(50)"
+            )
 
 
 class Branch(Base):
@@ -572,29 +620,41 @@ class Branch(Base):
     def __str__(self):
         return f"{self.code} - {self.third_category_code}"
 
+
 # # Set the data type of the primary key to VARCHAR in PostgreSQL
-if connection.vendor == 'postgresql':
+if connection.vendor == "postgresql":
     with connection.cursor() as cursor:
-        cursor.execute("""
+        cursor.execute(
+            """
             SELECT EXISTS(
                 SELECT * FROM information_schema.columns
                 WHERE table_name = 'employee_branch' AND column_name = 'code'
             )
-        """)
+        """
+        )
         column_exists = cursor.fetchone()[0]
         if column_exists:
-            cursor.execute("ALTER TABLE employee_branch ALTER COLUMN code TYPE VARCHAR(50)")
+            cursor.execute(
+                "ALTER TABLE employee_branch ALTER COLUMN code TYPE VARCHAR(50)"
+            )
+
 
 class Notch(models.Model):
     id = models.UUIDField(_("ID"), primary_key=True, editable=False, default=uuid.uuid4)
-    payroll_structure_code = models.CharField(_("Payroll Structure Code"), max_length=50,blank=True, null=True)
-    salary_grade = models.CharField(_("Salary Grade"), max_length=50, null=True, blank=True)
+    payroll_structure_code = models.CharField(
+        _("Payroll Structure Code"), max_length=50, blank=True, null=True
+    )
+    salary_grade = models.CharField(
+        _("Salary Grade"), max_length=50, null=True, blank=True
+    )
     no = models.CharField(_("No"), max_length=50, blank=True, null=True)
-    amount = models.DecimalField(_("Amount"), max_digits=8, decimal_places=2, null=True, blank=True)
+    amount = models.DecimalField(
+        _("Amount"), max_digits=8, decimal_places=2, null=True, blank=True
+    )
 
     def __str__(self):
         return f"{self.payroll_structure_code} - {self.salary_grade} - {self.no} - {self.amount}"
-    
+
     class Meta:
         verbose_name = "Notch"
         verbose_name_plural = "Notches"
@@ -602,20 +662,38 @@ class Notch(models.Model):
 
 class PayCategoryList(models.Model):
     no = models.CharField(_("No"), max_length=50, blank=True, null=True)
-    description = models.CharField(_("Description"), max_length=50, blank=True, null=True)
-    taxable_income_code = models.CharField(_("Taxable Income Code"), max_length=50, blank=True, null=True)
-    taxable_income_description = models.CharField(_("Taxable Income Description"), max_length=50, blank=True, null=True)
+    description = models.CharField(
+        _("Description"), max_length=50, blank=True, null=True
+    )
+    taxable_income_code = models.CharField(
+        _("Taxable Income Code"), max_length=50, blank=True, null=True
+    )
+    taxable_income_description = models.CharField(
+        _("Taxable Income Description"), max_length=50, blank=True, null=True
+    )
     tax_code = models.CharField(_("Tax Code"), max_length=50, blank=True, null=True)
-    tax_description = models.CharField(_("Tax Description"), max_length=50, blank=True, null=True)
-    gross_income_code = models.CharField(_("Gross Income Code"), max_length=50, blank=True, null=True)
-    gross_income_description = models.CharField(_("Gross Income Description"), max_length=50, blank=True, null=True)
-    bonus_tax_code = models.CharField(_("Bonus Tax Code"), max_length=50, blank=True, null=True)
-    bonus_tax_description = models.CharField(_("Bonus Tax Description"), max_length=50, blank=True, null=True)
-    currency_code = models.CharField(_("Currency Code"), max_length=50, blank=True, null=True)
+    tax_description = models.CharField(
+        _("Tax Description"), max_length=50, blank=True, null=True
+    )
+    gross_income_code = models.CharField(
+        _("Gross Income Code"), max_length=50, blank=True, null=True
+    )
+    gross_income_description = models.CharField(
+        _("Gross Income Description"), max_length=50, blank=True, null=True
+    )
+    bonus_tax_code = models.CharField(
+        _("Bonus Tax Code"), max_length=50, blank=True, null=True
+    )
+    bonus_tax_description = models.CharField(
+        _("Bonus Tax Description"), max_length=50, blank=True, null=True
+    )
+    currency_code = models.CharField(
+        _("Currency Code"), max_length=50, blank=True, null=True
+    )
 
     def __str__(self):
         return f"{self.no} - {self.description} - {self.taxable_income_code}"
-    
+
     class Meta:
         verbose_name = "Pay Category List"
         verbose_name_plural = "Pay Category Lists"
