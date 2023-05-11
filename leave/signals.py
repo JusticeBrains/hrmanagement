3d14061078ee957e96a32ec8e743810dbaaa23b4
JusticeBrains <justiceduodu14@gmail.com>
2023-05-11 12:22:52 +0000

    leave signal implemented

---
+++ leave/signals.py
@@ -0,0 +1,60 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import Employee
+from leave.models import LeaveRequest
+
+@receiver(post_save, sender=LeaveRequest)
+def update_employee_days_left(sender, instance, created, **kwargs):
+    if instance.hr_status == 2 and instance.hr_extension_status != 2 and instance.is_extend != 1:
+            if instance.leave_type.name == "Maternity":
+                instance.no_of_days_requested = instance.leave_type.max_number_of_days
+                instance.no_of_days_left = instance.employee.days_left
+
+            if instance.leave_type.name == "Medical":
+                instance.no_of_days_requested = instance.no_of_days_requested
+                employee = instance.employee
+                emp_days_left = employee.days_left
+                if emp_days_left is not None:
+                    if instance.no_of_days_requested <= emp_days_left:
+                        instance.no_of_days_left = emp_days_left - instance.no_of_days_requested
+
+
+            elif instance.leave_type.name == "Annual":
+                instance.no_of_days_requested = instance.no_of_days_requested
+
+                employee = instance.employee
+                emp_days_left = employee.days_left
+                if emp_days_left is not None:
+                    if instance.no_of_days_requested <= emp_days_left:
+                        instance.no_of_days_left = emp_days_left - instance.no_of_days_requested
+
+            no_of_days_exhausted = instance.employee.no_of_days_exhausted or 0
+            no_of_days_exhausted += instance.no_of_days_requested
+
+            # update employee with new values of days_left and no_of_days_exhausted
+            Employee.objects.filter(id=instance.employee.id).update(
+                days_left=instance.no_of_days_left, no_of_days_exhausted=no_of_days_exhausted
+            )
+        
+    if instance.hr_extension_status == 2 and instance.is_extend == 1 and instance.hr_status == 2:
+        if instance.leave_type.name == "Medical":
+            employee = instance.employee
+            emp_days_left = employee.days_left
+            if emp_days_left is not None:
+                if instance.no_of_extension_days <= emp_days_left:
+                    instance.no_of_days_left = emp_days_left - instance.no_of_extension_days
+
+        elif instance.leave_type.name == "Annual":
+            employee = instance.employee
+            emp_days_left = employee.days_left
+            if emp_days_left is not None:
+                if instance.no_of_extension_days <= emp_days_left:
+                    instance.no_of_days_left = emp_days_left - instance.no_of_extension_days
+
+        no_of_days_exhausted = instance.employee.no_of_days_exhausted or 0
+        no_of_days_exhausted += instance.no_of_extension_days
+
+        # update employee with new values of days_left and no_of_days_exhausted
+        Employee.objects.filter(id=instance.employee.id).update(
+            days_left=instance.no_of_days_left, no_of_days_exhausted=no_of_days_exhausted
+        )
