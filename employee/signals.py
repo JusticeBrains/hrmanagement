e87cada6a2bcf4199e4415e82f359c8735aca758
JusticeBrains <justiceduodu14@gmail.com>
2023-05-13 08:53:32 +0000

    updated signals

--- employee/signals.py
+++ employee/signals.py
@@ -2,11 +2,19 @@ from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 from employee.models import EmployeeAppraisal, Employee
 
-# @receiver(pre_save, sender=EmployeeAppraisal)
-# def update_employee_appraisal(sender, instance , created , **kwargs):
-#     employee = Employee.objects.get(id=instance.emp_code.id)
-#     instance.emp_name = employee.fullname
-#     instance.job_titles = employee.job_titles
-#     instance.department = employee.second_category_level
+@receiver(post_save, sender=EmployeeAppraisal)
+def update_employee_appraisal(sender, instance , **kwargs):
+        employee= Employee.objects.get(id=instance.emp_id.id)
+        instance.emp_name = employee.fullname
+        instance.employee_code = employee.code
+        instance.job_title = employee.job_titles
+        instance.department = employee.second_category_level
 
-# pre_save.connect(update_employee_appraisal, sender=EmployeeAppraisal)
+        
+        # Temporarily disconnect the signal receiver
+        post_save.disconnect(update_employee_appraisal, sender=EmployeeAppraisal)
+
+        instance.save(update_fields=['emp_name', 'employee_code', 'job_title', 'department'])
+
+        # Reconnect the signal receiver
+        post_save.connect(update_employee_appraisal, sender=EmployeeAppraisal)
