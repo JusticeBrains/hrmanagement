1d86386b9b6203734804b61b29e8eead2cb42cd1
JusticeBrains <justiceduodu14@gmail.com>
2023-05-15 03:08:20 +0000

    filterset_class implemented for property fields

--- calenders/views.py
+++ calenders/views.py
@@ -1,11 +1,19 @@
 
 from rest_framework import viewsets
+from django_property_filter import PropertyNumberFilter, PropertyFilterSet
 
 from .serializers import PeriodSerializer
 from .models import Period
 
+class PeriodFilter(PropertyFilterSet):
+    month = PropertyNumberFilter(field_name="month")
+    year = PropertyNumberFilter(field_name="year")
+
+    class Meta:
+        model = Period
+        fields = ["id", "date", "month", "year"]
 
 class PeriodViewSet(viewsets.ModelViewSet):
     queryset = Period.objects.all()
     serializer_class = PeriodSerializer
-    filterset_fields = ["id", "date", "month", "year"]
+    filterset_class = PeriodFilter
