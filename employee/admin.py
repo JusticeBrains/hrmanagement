1f5334f9f47b3ae9b400a4421ec6ac18e7cbd896
JusticeBrains <justiceduodu14@gmail.com>
2023-05-15 15:07:16 +0000

    refactored

--- employee/admin.py
+++ employee/admin.py
@@ -9,7 +9,7 @@ class EmployeeAdmin(admin.ModelAdmin):
 
 @admin.register(emodels.EmployeeAppraisalDetail)
 class EmployeeAppraisalDetailAdmin(admin.ModelAdmin):
-    list_display = ['emp_code', 'period', 'emp_name']
+    list_display = ['emp_code', 'period', 'emp_name', 'score']
 
 admin.site.register(emodels.StaffCategory)
 admin.site.register(emodels.Branch)
