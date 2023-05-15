4f7e3622b724ce4168a49e8dfeb19a4a31d63d7d
JusticeBrains <justiceduodu14@gmail.com>
2023-05-15 12:03:30 +0000

    updated admin

--- employee/admin.py
+++ employee/admin.py
@@ -6,12 +6,16 @@ from employee import models as emodels
 class EmployeeAdmin(admin.ModelAdmin):
     search_fields = ['code']
 
+
+@admin.register(emodels.EmployeeAppraisalDetail)
+class EmployeeAppraisalDetailAdmin(admin.ModelAdmin):
+    list_display = ['emp_code', 'period', 'emp_name']
+
 admin.site.register(emodels.StaffCategory)
 admin.site.register(emodels.Branch)
 admin.site.register(emodels.Unit)
 admin.site.register(emodels.Department)
 admin.site.register(emodels.Notch)
 admin.site.register(emodels.PayCategoryList)
-admin.site.register(emodels.EmployeeAppraisalDetail)
 admin.site.register(emodels.AppraisalGrading)
 admin.site.register(emodels.EmployeeAppraisal)
