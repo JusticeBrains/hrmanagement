ab86c6a43e6226bf095ec2847e860266c4adab35
JusticeBrains <justiceduodu14@gmail.com>
2023-06-02 22:50:13 +0000

    paygroup  refractored

--- paygroup/models.py
+++ paygroup/models.py
@@ -1,34 +1,31 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from options import text_options
-
+from company.models import Company
 
 class PayGroup(models.Model):
-    no = models.CharField(_("No."), max_length=50)
-    description = models.TextField(_("Description"))
-    short_name = models.CharField(_("Short Name"), max_length=50)
-    overtime_code = models.ForeignKey("CalculationHeader", verbose_name=_("Overtime Code"), on_delete=models.CASCADE)
-    overtime_description = models.TextField(_("Overtime Description"))
-    limit_amount = models.DecimalField(_("Limit Amount"), max_digits=5, decimal_places=2)
-    limit_percent = models.DecimalField(_("Limit Percent(Basic)"), max_digits=5, decimal_places=2)
-    taxable_inocme_code = models.CharField(_("Taxable Income Code"), max_length=50)
-    taxable_inocme_description = models.TextField(_("Taxable Income Description"))
-    tax_code = models.ForeignKey("TaxLaws", verbose_name=_("Tax Code"), on_delete=models.CASCADE, related_name="tax_code")
-    tax_description = models.CharField(_("Tax Description"), max_length=150)
-    tax_relief_code = models.ForeignKey("TaxReliefs", verbose_name=_("Tax Reliefs"), on_delete=models.CASCADE)
-    tax_relief_name = models.CharField(_("Tax Relief Name"), max_length=150)
-    gross_income_code = models.CharField(_("CalculationHeader"), max_length=50)
-    gross_income_description = models.CharField(_("Gross Income Description"), max_length=150)
-    currency_code = models.ForeignKey("Currency", verbose_name=_("Currency Code"), on_delete=models.CASCADE)
-    bonus_tax_code = models.ForeignKey("TaxLaws", verbose_name=_("Bonus Tax Code"), on_delete=models.CASCADE, related_name="bonus_tax_code")
-    bonus_tax_description = models.CharField(_("Bonus Tax Description"), max_length=150)
-    commission_percentage = models.DecimalField(_("Commission Percentage"), max_digits=5, decimal_places=2)
-    tax_percentage_on_commission = models.DecimalField(_("Tax Percentage on Commission"), max_digits=5, decimal_places=2)
-    gross_up = models.BooleanField(_("Gross Up"))
+    no = models.CharField(_("No."), max_length=50, null=True, blank=True)
+    description = models.CharField(_("Description"), max_length=100,null=True, blank=True)
+    taxable_income_code = models.CharField(_("Taxable Income Code"), max_length=50, null=True, blank=True)
+    taxable_income_description = models.CharField(_("Taxable Income Description"), max_length=100,null=True, blank=True)
+    tax_code = models.CharField(verbose_name=_("Tax Code"), null=True, blank=True)
+    tax_description = models.CharField(_("Tax Description"), max_length=150,  null=True, blank=True)
+    gross_income_code = models.CharField(_("CalculationHeader"), max_length=50,  null=True, blank=True)
+    gross_income_description = models.CharField(_("Gross Income Description"), max_length=150,  null=True, blank=True)
+    currency_code = models.CharField(verbose_name=_("Currency Code"), null=True, blank=True)
+    bonus_tax_code = models.CharField(verbose_name=_("Bonus Tax Code"),  null=True, blank=True)
+    bonus_tax_description = models.CharField(_("Bonus Tax Description"), max_length=150,  null=True, blank=True)
+    gross_up = models.BooleanField(_("Gross Up"),  null=True, blank=True)
+    company = models.CharField(_("Company"), max_length=150, null=True, blank=True)
+    company_id = models.ForeignKey(Company, verbose_name=_("Company ID"), on_delete=models.CASCADE, null=True, blank=True)
+
 
     class Meta:
         verbose_name = "Pay Group"
         verbose_name_plural = "Pay Groups"
+    
+    def __str__(self):
+        return f"{self.no} - {self.description} - {self.taxable_income_code}"
 
 
 class CalculationHeader(models.Model):
