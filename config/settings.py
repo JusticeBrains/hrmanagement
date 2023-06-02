dcbcb2ad5a6e9f3cfe95839ee1789de173201489
JusticeBrains <justiceduodu14@gmail.com>
2023-06-02 12:07:45 +0000

    refactored

--- config/settings.py
+++ config/settings.py
@@ -12,8 +12,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
-from decouple import config
-import dj_database_url
+from environs import Env
+
+env = Env()
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -104,17 +106,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASE_URL = config('DATABASE_URL')
 
 DATABASES = {
-       'default':{
-        'ENGINE':'django.db.backends.postgresql_psycopg2',
-        'NAME': config('PGNAME'),
-        'USER': config('PGUSER'),
-        'PASSWORD':config('PGPASSWORD'),
-        'HOST':config('PGHOST'),
-        'PORT':config("PGPORT")
-    }
+    'default': env.dj_db_url("DATABASE_URL")
 }
 
 
