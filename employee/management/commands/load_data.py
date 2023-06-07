c0399af3623ae527d11a88395722b274feb36226
JusticeBrains <justiceduodu14@gmail.com>
2023-06-07 22:09:44 +0000

    Update load_data.py

--- employee/management/commands/load_data.py
+++ employee/management/commands/load_data.py
@@ -191,15 +191,15 @@ class Command(BaseCommand):
             #     get_user_data(url=env.str("reiss_co"), auth=auth, company=company.name, company_id=company.id)
             #     self.stdout.write(self.style.SUCCESS("Successfully load data to database"))
 
-            if company.name == "INTERCITY STC COACHES LTD - JUNIOR STAFF":
-                self.stdout.write(self.style.SUCCESS(f"Starting load data to database {company.id} -- {company.name}"))        
-                get_user_data(url=env.str("intercity_jun"), auth=auth, company=company.name, company_id=company.id)
-                self.stdout.write(self.style.SUCCESS("Successfully load data to database"))
-
-            # if company.name == "INTERCITY STC COACHES LTD - SENIOR STAFF":
+            # if company.name == "INTERCITY STC COACHES LTD - JUNIOR STAFF":
             #     self.stdout.write(self.style.SUCCESS(f"Starting load data to database {company.id} -- {company.name}"))        
-            #     get_user_data(url=env.str("intercity_sen"), auth=auth, company=company.name, company_id=company.id)
+            #     get_user_data(url=env.str("intercity_jun"), auth=auth, company=company.name, company_id=company.id)
             #     self.stdout.write(self.style.SUCCESS("Successfully load data to database"))
+
+            if company.name == "INTERCITY STC COACHES LTD - SENIOR STAFF":
+                self.stdout.write(self.style.SUCCESS(f"Starting load data to database {company.id} -- {company.name}"))        
+                get_user_data(url=env.str("intercity_sen"), auth=auth, company=company.name, company_id=company.id)
+                self.stdout.write(self.style.SUCCESS("Successfully load data to database"))
 
             # if company.name == "INTERCITY STC COACHES LTD - DRIVERS":
             #     self.stdout.write(self.style.SUCCESS(f"Starting load data to database {company.id} -- {company.name}"))        
