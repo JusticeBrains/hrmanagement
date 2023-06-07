3c11e94aae184a8de619c7837a496996b736fbdb
JusticeBrains <justiceduodu14@gmail.com>
2023-06-07 21:37:36 +0000

    Update load_data.py

--- employee/management/commands/load_data.py
+++ employee/management/commands/load_data.py
@@ -176,10 +176,10 @@ class Command(BaseCommand):
                 get_user_data(url=env.str("baf"), auth=auth, company=company.name, company_id=company.id)
                 self.stdout.write(self.style.SUCCESS("Successfully load data to database"))
 
-            # elif company.name == "FAAB Systems Gh. Ltd":
-            #     self.stdout.write(self.style.SUCCESS(f"Starting load data to database {company.id} -- {company.name}"))        
-            #     get_user_data(url=env.str("faab"), auth=auth, company=company.name, company_id=company.id)
-            #     self.stdout.write(self.style.SUCCESS("Successfully load data to database"))
+            elif company.name == "FAAB Systems Gh. Ltd":
+                self.stdout.write(self.style.SUCCESS(f"Starting load data to database {company.id} -- {company.name}"))        
+                get_user_data(url=env.str("faab"), auth=auth, company=company.name, company_id=company.id)
+                self.stdout.write(self.style.SUCCESS("Successfully load data to database"))
 
             # elif company.name == "Rock City Hotel Heads of Department":
             #     self.stdout.write(self.style.SUCCESS(f"Starting load data to database {company.id} -- {company.name}"))        
