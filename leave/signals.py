fefb67b2790ccd03c11105b9da08b9ee72c11c04
JusticeBrains <justiceduodu14@gmail.com>
2023-06-07 15:50:46 +0000

    leaveplan compuatation added

--- leave/signals.py
+++ leave/signals.py
@@ -1,7 +1,7 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import Employee
-from leave.models import LeaveRequest, LeaveType
+from leave.models import LeaveRequest, LeaveType, LeavePlan
 
 @receiver(post_save, sender=LeaveRequest)
 def update_employee_days_left(sender, instance, created, **kwargs):
@@ -48,6 +48,36 @@ def update_employee_days_left(sender, instance, created, **kwargs):
     post_save.connect(update_employee_days_left, sender=LeaveRequest)
 
 
+
+@receiver(post_save, sender=LeavePlan)
+def update_employee_days_left_leave_plan(sender, instance, created, **kwargs):
+    if instance.hr_status == 1 :
+            if instance.leave_type.name == "Maternity":
+                instance.no_of_days_requested = instance.leave_type.max_number_of_days
+                instance.plan_days_left = instance.employee.plan_days_left
+
+            else:
+                # instance.no_of_days_requested = instance.no_of_days_requested
+                max_days = instance.leave_type.max_number_of_days
+                employee = instance.employee
+                emp_days_left = employee.plan_days_left
+                if emp_days_left is not None:
+                    if instance.no_of_days_requested <= emp_days_left and instance.no_of_days_requested <= max_days:
+                        instance.plan_days_left = emp_days_left - instance.no_of_days_requested
+
+            no_of_days_exhausted = instance.employee.plan_no_of_days_exhausted or 0
+            no_of_days_exhausted += instance.no_of_days_requested
+
+            # update employee with new values of days_left and no_of_days_exhausted
+            Employee.objects.filter(id=instance.employee.id).update(
+                days_left=instance.no_of_days_left, plan_no_of_days_exhausted=no_of_days_exhausted
+            )
+    
+    post_save.disconnect(update_employee_days_left_leave_plan, sender=LeavePlan)
+    instance.save()
+    post_save.connect(update_employee_days_left_leave_plan, sender=LeavePlan)
+
+
 @receiver(post_save, sender=LeaveType)
 def update_paygroup_code(sender, instance, created, **kwargs):
     if created:
@@ -57,4 +87,3 @@ def update_paygroup_code(sender, instance, created, **kwargs):
 
 def send_leave_return_reminder(sender, instance, **kwargs):
     leave_requests = LeaveRequest.objects.all()
-    
