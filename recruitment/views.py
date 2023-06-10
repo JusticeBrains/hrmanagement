dd3db52018f79a226beb2f74504e42d87dc5b77a
JusticeBrains <justiceduodu14@gmail.com>
2023-06-10 17:35:27 +0000

    Update views.py

--- recruitment/views.py
+++ recruitment/views.py
@@ -18,15 +18,7 @@ from .serializers import (
 class EmployeeRequisitionViewSet(viewsets.ModelViewSet):
     queryset = EmployeeRequisition.objects.all()
     serializer_class = EmployeeRequisitionSerializer
-    filterset_fields = [
-        "id",
-        "department",
-        "position",
-        "no_of_vacancies",
-        "status",
-        "published",
-        "company",
-    ]
+    filterset_fields = "__all__"
 
 
 class JobApplicationViewSet(viewsets.ModelViewSet):
