54d2dcf2f207cc603793096ac0b87bf5d9e6e718
JusticeBrains <justiceduodu14@gmail.com>
2023-06-23 09:04:45 +0000

    removed DecimalRangeField

--- employee/models.py
+++ employee/models.py
@@ -8,7 +8,7 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth import get_user_model
 from django.utils import timezone
-from django.contrib.postgres.fields import IntegerRangeField, DecimalRangeField
+from django.contrib.postgres.fields import IntegerRangeField
 
 User = get_user_model()
 
