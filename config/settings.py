5c42d2b2c1a0bd3bdcb4f27fb508fcbeb84fab6f
JusticeBrains <justiceduodu14@gmail.com>
2023-08-09 17:19:51 +0000

    change pagination size

--- config/settings.py
+++ config/settings.py
@@ -154,8 +154,8 @@ REST_FRAMEWORK = {
     ),
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
-    "PAGE_SIZE": 50,
-}
+    "PAGE_SIZE": 100,
+} 
 
 
 SITE_ID = 1
