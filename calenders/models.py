34b5e29b3648a081da07a3f55d0c9e9672b8e0e0
JusticeBrains <justiceduodu14@gmail.com>
2023-08-14 09:17:03 +0000

    calender modle removed

--- calenders/models.py
+++ calenders/models.py
@@ -1,25 +1,37 @@
-from django.db import models
+# import uuid
+# from django.db import models
 
-from django.utils.translation import gettext_lazy as _
+# from django.utils.translation import gettext_lazy as _
 
-class Period(models.Model):
-    date = models.DateField(_("Date"))
-    active = models.BooleanField(_("Active"), blank=True, null=True)
 
-    @property
-    def month(self):
-        return self.date.month
-    
-    @property
-    def year(self):
-        return self.date.year
-    
-    class Meta:
-        verbose_name = "Period"
-        verbose_name_plural = "Periods"
-    
-    def __str__(self) -> str:
-        return f"{self.month},{self.year}"
-    
-    def __repr__(self) -> str:
-        return f"{self.month},{self.year}"
+# class Period(models.Model):
+#     id = models.UUIDField(_("ID"), primary_key=True, editable=False, default=uuid.uuid4)
+#     code = models.CharField(_("Code"), max_length=50, blank=True, null=True)
+#     period_no = models.CharField(_("Period No"), max_length=50, blank=True, null=True)
+#     period_name = models.CharField(
+#         _("Period Name"), max_length=150, blank=True, null=True
+#     )
+#     no_of_days = models.PositiveIntegerField(_("No Of Days"), blank=True, null=True)
+#     no_of_hours = models.PositiveIntegerField(_("No Of Hours"), blank=True, null=True)
+#     no_of_hours_per_day = models.PositiveIntegerField(
+#         _("No Of Hours Per Day"), blank=True, null=True
+#     )
+#     no_of_working_days = models.PositiveIntegerField(
+#         _("No Of Working Days"), blank=True, null=True
+#     )
+#     calender_year = models.PositiveIntegerField(
+#         _("Calender Year"), blank=True, null=True
+#     )
+#     closed = models.BooleanField(_("Closed"), default=False)
+#     start_date = models.DateField(_("Start Date"), blank=True, null=True)
+#     end_date = models.DateField(_("End Date"), blank=True, null=True)
+
+#     class Meta:
+#         verbose_name = "Period"
+#         verbose_name_plural = "Periods"
+
+#     def __str__(self) -> str:
+#         return f"{self.code}"
+
+#     def __repr__(self) -> str:
+#         return f"{self.code}"
--- calenders/serializers.py
+++ calenders/serializers.py
@@ -1,22 +1,9 @@
-from rest_framework import serializers
+# from rest_framework import serializers
 
-from .models import Period
+# from .models import Period
 
 
-class PeriodSerializer(serializers.ModelSerializer):
-    date = serializers.DateField()
-    active = serializers.BooleanField()
-    month = serializers.ReadOnlyField()
-    year = serializers.ReadOnlyField()
-
-    class Meta:
-        model = Period
-        fields = "__all__"
-
-    @property
-    def get_month(self, obj):
-        return obj.date.month
-    
-    @property
-    def get_year(self, obj):
-        return obj.data.year
+# class PeriodSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Period
+#         fields = "__all__"
