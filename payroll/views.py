3e1e3cd517f5a8ab52d4a4e121051327733ec0d7
JusticeBrains <justiceduodu14@gmail.com>
2023-08-16 10:35:46 +0000

    model fields added to filterset

--- payroll/views.py
+++ payroll/views.py
@@ -7,8 +7,10 @@ from .serializers import TransactionSerializer, SavingSchemeSerializer
 class TransactionViewSet(viewsets.ModelViewSet):
     queryset = Transactions.objects.all()
     serializer_class = TransactionSerializer
+    filterset_fields = "__all__"
 
 
 class SavingSchemeViewSet(viewsets.ModelViewSet):
     queryset = SavingScheme.objects.all()
     serializer_class = SavingSchemeSerializer
+    filterset_fields = "__all__"
