752c72dacc0ecbce8d11d2f3b0b4901c242b87c4
JusticeBrains <justiceduodu14@gmail.com>
2023-08-17 16:57:00 +0000

    Update apps.py

--- calenders/apps.py
+++ calenders/apps.py
@@ -5,5 +5,5 @@ class CalendersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'calenders'
 
-    # def ready(self) -> None:
-    #     import calenders.signals
+    def ready(self) -> None:
+        import calenders.signals
