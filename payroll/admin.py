8ddd5174ea7a6c2b1dbc84aa49afed6af831cae3
JusticeBrains <justiceduodu14@gmail.com>
2023-08-18 14:44:41 +0000

    TransactionEntries and SavingSchemeEntries resgistered to admin

--- payroll/admin.py
+++ payroll/admin.py
@@ -1,13 +1,27 @@
 from django.contrib import admin
 
-from .models import Transactions, SavingScheme
+from .models import Transactions, SavingScheme, TransactionEntries, SavingSchemeEntries
+
 
 @admin.register(Transactions)
 class TransactionAdmin(admin.ModelAdmin):
     list_display = ["id", "code"]
     list_filter = ["id", "code"]
 
+
 @admin.register(SavingScheme)
 class SavingSchemeAdmin(admin.ModelAdmin):
     list_display = ["id", "code"]
     list_filter = ["id", "code"]
+
+
+@admin.register(SavingSchemeEntries)
+class SavingSchemeEntriesAdmin(admin.ModelAdmin):
+    list_display = ["id", "disbursement_type"]
+    list_filter = ["id", "employee"]
+
+
+@admin.register(TransactionEntries)
+class TransactionEntriesAdmin(admin.ModelAdmin):
+    list_display = ["id", "disbursement_type"]
+    list_filter = ["id", "transaction_code"]
