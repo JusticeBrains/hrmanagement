706e080175f998d7813c4c4b800cb3bd4a608db6
JusticeBrains <justiceduodu14@gmail.com>
2023-08-28 15:06:40 +0000

    Update signals.py

--- users/signals.py
+++ users/signals.py
@@ -13,38 +13,39 @@ from company.models import Company
 env = Env()
 env.read_env()
 
-# @receiver(post_save, sender=CustomUser)
-# def user_created(sender, instance, created, **kwargs):
-#     post_save.disconnect(user_created, sender=CustomUser)
-#     if created:
-#         pass_gen = instance.generated_pass
-#         try:
-#             print("---------------Sending -----------------------")
-#             subject = "Login Credentials"
-#             message = (
-#                 f"Hello {instance.first_name} your password to signin is {instance.generated_pass}"
-#             )
-#             from_email = env.str("EMAIL_USER")
-#             recipient_list = [
-#                 instance.email,
-#                 from_email
-#             ]
-#             for email in recipient_list:
-#                 if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
-#                     raise ValueError(f"Invalid email address: {email}")
-#             send_mail(subject, message, from_email, recipient_list)
-#             instance.generated_pass = make_password(pass_gen)
-#             instance.save()
-#             print("---------------Sent -----------------------")
-#         except ValueError as ve:
-#             print(f"Error occurred while sending email: {str(ve)}")
+@receiver(post_save, sender=CustomUser)
+def user_created(sender, instance, created, **kwargs):
+    post_save.disconnect(user_created, sender=CustomUser)
+    if instance:
+        pass_gen = instance.generated_pass
+        try:
+            print("---------------Sending -----------------------")
+            subject = "Login Credentials"
+            message = (
+                f"Hello {instance.first_name} your password to signin is {instance.generated_pass}"
+            )
+            from_email = env.str("EMAIL_USER")
+            recipient_list = [
+                instance.email,
+                from_email
+            ]
+            for email in recipient_list:
+                if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+                    raise ValueError(f"Invalid email address: {email}")
+            send_mail(subject, message, from_email, recipient_list)
+            instance.generated_pass = make_password(pass_gen)
+            instance.save()
+            print("---------------Sent -----------------------")
+        except ValueError as ve:
+            print(f"Error occurred while sending email: {str(ve)}")
 
-#         except Exception as e:
-#             print("Error occurred while sending email:")
-#             print(str(e))
-#             traceback.print_exc()
+        except Exception as e:
+            print("Error occurred while sending email:")
+            print(str(e))
+            traceback.print_exc()
+        instance.save()
 
-#     post_save.connect(user_created, sender=CustomUser)
+    post_save.connect(user_created, sender=CustomUser)
 
 
 @receiver(post_save, sender=CustomUser)
