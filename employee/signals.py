c917aa85f97ab0ddfc3f32264d4c6a1310a40b05
JusticeBrains <justiceduodu14@gmail.com>
2023-08-28 10:32:21 +0000

    Update signals.py

--- employee/signals.py
+++ employee/signals.py
@@ -289,13 +289,13 @@ def update_emp_total_score_scores(sender, instance, **kwargs):
                 (instance.final_score / 100) * instance.score_on_target, ndigits=2
             )
 
-@receiver(post_save, sender=Branch)
-def updated_fields_branch(sender, instance,created, **kwargs):
-    post_save.disconnect(updated_fields_branch, sender=Branch)
-    if created:
-        instance.unit_name = instance.unit.name if instance.unit.name is not None else None 
-        instance.save()
-    post_save.connect(updated_fields_branch, sender=Branch)
+# @receiver(post_save, sender=Branch)
+# def updated_fields_branch(sender, instance,created, **kwargs):
+#     post_save.disconnect(updated_fields_branch, sender=Branch)
+#     if created:
+#         instance.unit_name = instance.unit.name if instance.unit.name is not None else None 
+#         instance.save()
+#     post_save.connect(updated_fields_branch, sender=Branch)
 
 
 @receiver(post_save, sender=Unit)
