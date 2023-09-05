a6df708debebb7308a2c597a24c9e98342fcf16d
JusticeBrains <justiceduodu14@gmail.com>
2023-09-05 12:30:59 +0000

    Update views.py

--- payroll/views.py
+++ payroll/views.py
@@ -89,7 +89,30 @@ class OvertimeEntriesViewSet(viewsets.ModelViewSet):
 class LoansViewSet(viewsets.ModelViewSet):
     queryset = Loans.objects.all()
     serializer_class = LoansSerializer
-    filterset_fields = "__all__"
+    filterset_fields = [
+        "id",
+        "loan",
+        "loan_name",
+        "description",
+        "amount",
+        "employee",
+        "employee_code",
+        "employee_name",
+        "monthly_repayment",
+        "total_amount_paid",
+        "duration",
+        "company",
+        "company_name",
+        "status",
+        "created_at",
+        "transaction_period",
+        "transaction_period_code",
+        "deduction_start_period",
+        "deduction_start_period_code",
+        "deduction_end_period",
+        "deduction_end_period_code",
+        "user_id",
+    ]
 
 
 class LoanEntriesViewSet(viewsets.ModelViewSet):
@@ -133,20 +156,24 @@ class EmployeeShiftEntriesViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = EmployeeShiftEntriesSerializer
     filterset_fields = "__all__"
 
+
 class PaymasterViewSet(viewsets.ModelViewSet):
     queryset = Paymaster.objects.all()
     serializer_class = PaymasterSerializer
     filterset_fields = "__all__"
+
 
 class TaxLawsViewSet(viewsets.ModelViewSet):
     queryset = TaxLaws.objects.all()
     serializer_class = TaxLawsSerializer
     filterset_fields = "__all__"
 
+
 class TaxLawTypeViewSet(viewsets.ModelViewSet):
     queryset = TaxLawType.objects.all()
     serializer_class = TaxLawTypeSerializer
     filterset_fields = "__all__"
+
 
 class TaxReliefViewSet(viewsets.ModelViewSet):
     queryset = TaxRelief.objects.all()
