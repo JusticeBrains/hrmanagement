64ccd7f499c57c701f6947c28de158ea904e4eab
JusticeBrains <justiceduodu14@gmail.com>
2023-09-05 15:17:26 +0000

    Update signals.py

--- calenders/signals.py
+++ calenders/signals.py
@@ -80,15 +80,16 @@ def process_payroll(sender, instance, **kwargs):
                 )
                 amount_to_be_paid = (
                     min(monthly_amount, total_paid)
-                    if total_paid != 0
+                    if total_paid == 0
                     else monthly_amount
                 )
-                if emp_loan.total_amount_paid is not None:
-                    emp_loan.total_amount_paid += amount_to_be_paid
-                    emp_loan.monthly_repayment = amount_to_be_paid
-                elif emp_loan.total_amount_paid is None:
-                    emp_loan.total_amount_paid = amount_to_be_paid
-                    emp_loan.monthly_repayment = amount_to_be_paid
+                if instance.status == 2:
+                    if emp_loan.total_amount_paid is not None:
+                        emp_loan.total_amount_paid += amount_to_be_paid
+                        emp_loan.monthly_repayment = amount_to_be_paid
+                    elif emp_loan.total_amount_paid is None:
+                        emp_loan.total_amount_paid = amount_to_be_paid
+                        emp_loan.monthly_repayment = amount_to_be_paid
 
                 total_loan_deductions += amount_to_be_paid
                 emp_loan.save()
