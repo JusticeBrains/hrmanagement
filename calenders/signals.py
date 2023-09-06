581138115363b2e8b5ae90a65d492caada23d377
JusticeBrains <justiceduodu14@gmail.com>
2023-09-06 11:57:11 +0000

    Update signals.py

--- calenders/signals.py
+++ calenders/signals.py
@@ -124,7 +124,6 @@ def process_payroll(sender, instance, **kwargs):
                             else:
                                 emp_loan.total_amount_paid = amount_to_be_paid
                                 emp_loan.monthly_repayment = amount_to_be_paid
-                            emp_loan.save()
                             loan_dict.append(
                                 {
                                     "loan_name": emp_loan.loan_name,
@@ -132,6 +131,8 @@ def process_payroll(sender, instance, **kwargs):
                                     "total_amount_paid": float(emp_loan.total_amount_paid),
                                 }
                             )
+                            emp_loan.save()
+                            
 
                         total_loan_deductions += amount_to_be_paid
 
