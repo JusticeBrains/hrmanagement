5b47eeb834644cc78c877833fc30103c877b8964
JusticeBrains <justiceduodu14@gmail.com>
2023-09-07 11:35:36 +0000

    Update signals.py

--- calenders/signals.py
+++ calenders/signals.py
@@ -233,9 +233,8 @@ def process_payroll(sender, instance, **kwargs):
                             emp_loan.save()
 
                 net_income = gross_income - (total_deductions + Decimal(total_loan_deductions))
-                total_deductions += (
-                    Decimal(total_loan_deductions) if total_loan_deductions is not None else 0
-                )
+                total_deductions += Decimal(total_loan_deductions) + Decimal(total_contribution)
+                
                 employee.net_salary = net_income
                 employee.gross_salary = gross_income
                 payslip.append(
@@ -244,7 +243,7 @@ def process_payroll(sender, instance, **kwargs):
                         "gross_salary": float(gross_income),
                         "net_salary": float(net_income),
                         "total_deductions": float(total_deductions + total_contribution),
-                        "saving_scheme": float(total_contribution),
+                        "saving_scheme_contribution": float(total_contribution),
                         "total_allowances": float(total_allowances),
                         "total_loan_deductions": float(total_loan_deductions),
                         "total_loan_amount": float(total_loan_amount),
