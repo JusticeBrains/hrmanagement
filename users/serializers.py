d9c9b917ceeaf01661ccbd8d6dc8563d59e4cb7b
JusticeBrains <justiceduodu14@gmail.com>
2023-09-11 21:17:40 +0000

    Update serializers.py

--- users/serializers.py
+++ users/serializers.py
@@ -1,26 +1,36 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from django.db import transaction
+
 User = get_user_model()
 
 
 class CustomUserSerializer(serializers.ModelSerializer):
     company_names = serializers.ReadOnlyField()
+
     class Meta:
         model = User
-        exclude = ['user_permissions', 'groups','is_superuser','last_login', 'date_joined',]
+        exclude = [
+            "user_permissions",
+            "groups",
+            "is_superuser",
+            "last_login",
+            "date_joined",
+        ]
 
     def create(self, validated_data):
-        # Perform the user creation logic
+        companies_data = validated_data.pop("companies", [])  # Extract companies data
         user = User.objects.create(**validated_data)
 
-        # Update the company_names field
-        if user.companies.exists():
-            company_dicts = []
-            related_companies = user.companies.all()
+        if companies_data:
+            user.companies.set(
+                companies_data
+            )  # Use set() to update the many-to-many relationship
 
-            for company in related_companies:
-                company_dicts.append({"company_id": str(company.id), "name": company.name})
+            company_dicts = [
+                {"company_id": str(company.id), "name": company.name}
+                for company in companies_data
+            ]
 
             with transaction.atomic():
                 user.company_names = [{"companies": company_dicts}]
