7b3bdd3ae60ddb21f0d57911bebf32c89e113550
JusticeBrains <justiceduodu14@gmail.com>
2023-09-12 09:19:41 +0000

    payroll model removed

--- payroll/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class PayrollConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'payroll'
-
-    def ready(self):
-        from payroll import signals
