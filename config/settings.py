e4e9fa1cef7c9a2bd66f51990aeda47a1d2c4924
JusticeBrains <justiceduodu14@gmail.com>
2023-09-12 09:11:17 +0000

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -57,7 +57,6 @@ LOCAL_APPS = [
     "leave.apps.LeaveConfig",
     "calenders.apps.CalendersConfig",
     "recruitment.apps.RecruitmentConfig",
-    "payroll.apps.PayrollConfig",
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
